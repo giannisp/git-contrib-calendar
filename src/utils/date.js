910147b6723f5729a9084936ea88c6ba6e5983bc
Ioannis Poulakas <giannis.p@gmail.com>
2020-08-15 22:31:58 +0300

    Add date util

---
+++ src/utils/date.js
@@ -0,0 +1,50 @@
+/**
+ * @file Date helpers.
+ */
+
+const moment = require('moment');
+const groupBy = require('lodash.groupby');
+
+exports.DAY_NAMES = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+exports.MONTH_NAMES = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
+
+/**
+ * Return days grouped by weekday for calendar display purposes.
+ *
+ * @return {Object[]} The day data.
+ */
+const getCalendarDays = () => {
+  const now = moment();
+  const startDate = now.clone().subtract(1, 'years').isoWeekday(0);
+
+  const days = [];
+  const day = startDate.clone();
+
+  while (day <= now) {
+    days.push({
+      dayIndex: day.day(),
+      monthIndex: day.month(),
+      year: day.year(),
+      day: day.clone(),
+    });
+    day.add(1, 'days');
+  }
+
+  return { totalDays: days.length, days: groupBy(days, 'dayIndex') };
+};
+
+exports.getCalendarDays = getCalendarDays;
