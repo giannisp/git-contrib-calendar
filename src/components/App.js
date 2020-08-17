bb6d2e07c44fe0edd91a3702c3641866d3ccafad
Ioannis Poulakas <giannis.p@gmail.com>
2020-08-17 22:42:18 +0300

    Remove redundant fetchBranch

--- src/components/App.js
+++ src/components/App.js
@@ -7,38 +7,18 @@ const PropTypes = require('prop-types');
 const { Box } = require('ink');
 const importJsx = require('import-jsx');
 
-const { getBranch } = require('../services/git');
-
 const RepoInfo = importJsx('./RepoInfo');
 const Calendar = importJsx('./Calendar');
 
-const App = ({ repoPath, author }) => {
-  const [branch, setBranch] = React.useState(null);
-
-  React.useEffect(() => {
-    const fetchBranch = async () => {
-      const branchData = await getBranch(repoPath);
-
-      setBranch(branchData);
-    };
-
-    fetchBranch();
-  }, []);
-
-  if (!branch) {
-    return null;
-  }
-
-  return (
-    <Box flexDirection="column">
-      <Box marginBottom={1}>
-        <RepoInfo repoPath={repoPath} />
-      </Box>
-
-      <Calendar repoPath={repoPath} author={author} />
+const App = ({ repoPath, author }) => (
+  <Box flexDirection="column">
+    <Box marginBottom={1}>
+      <RepoInfo repoPath={repoPath} />
     </Box>
-  );
-};
+
+    <Calendar repoPath={repoPath} author={author} />
+  </Box>
+);
 
 App.propTypes = {
   repoPath: PropTypes.string.isRequired,
