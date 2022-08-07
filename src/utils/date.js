471716b0fdcbaa9a54ea6ca9bff70b5b299efd9d
Ioannis Poulakas <giannis.p@gmail.com>
2022-08-07 09:41:38 +0300

    Migrate date helpers from moment to luxon

--- src/utils/date.js
+++ src/utils/date.js
@@ -2,7 +2,7 @@
  * @file Date helpers.
  */
 
-const moment = require('moment');
+const { DateTime, Interval } = require('luxon');
 
 exports.DAY_NAMES = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
@@ -21,7 +21,7 @@ exports.MONTH_NAMES = [
   'Dec',
 ];
 
-const DATE_FORMAT = 'YYYY-MM-DD';
+const DATE_FORMAT = 'yyyy-MM-dd';
 
 /**
  * Return the date range for the calendar.
@@ -31,18 +31,21 @@ const DATE_FORMAT = 'YYYY-MM-DD';
  * @return {Object} The date range data.
  */
 const getDateRange = (year) => {
+  let dateFrom = null;
+  let dateTo = null;
+
   if (year) {
-    return {
-      from: moment(`${year}-01-01`).isoWeekday(7).format(DATE_FORMAT),
-      to: moment(`${year}-12-31`).format(DATE_FORMAT),
-    };
+    dateFrom = DateTime.fromObject({ year });
+    dateTo = dateFrom.endOf('year');
+  } else {
+    const now = DateTime.now();
+    dateFrom = now.minus({ years: 1 });
+    dateTo = now;
   }
 
-  const now = moment();
-
   return {
-    from: now.clone().subtract(1, 'years').isoWeekday(7).format(DATE_FORMAT),
-    to: now.format(DATE_FORMAT),
+    from: dateFrom.endOf('week').toFormat(DATE_FORMAT),
+    to: dateTo.toFormat(DATE_FORMAT),
   };
 };
 
@@ -57,22 +60,19 @@ exports.getDateRange = getDateRange;
  * @return {Object[]} The day data.
  */
 const getCalendarDays = (dateFrom, dateTo) => {
-  const dateToMoment = moment(dateTo);
+  const interval = Interval.fromDateTimes(
+    DateTime.fromFormat(dateFrom, DATE_FORMAT),
+    DateTime.fromFormat(dateTo, DATE_FORMAT).plus({ days: 1 }),
+  );
 
-  const days = [];
-  const day = moment(dateFrom);
+  const intervalDays = interval.splitBy({ days: 1 }).map((day) => day.start);
 
-  while (day <= dateToMoment) {
-    days.push({
-      dayIndex: day.day(),
-      monthIndex: day.month(),
-      year: day.year(),
-      date: day.format('YYYY-MM-DD'),
-    });
-    day.add(1, 'days');
-  }
-
-  return days;
+  return intervalDays.map((intervalDay) => ({
+    dayIndex: intervalDay.weekday % 7, // convert from 1-7 to 0-6 (0 is Sun)
+    monthIndex: intervalDay.month - 1,
+    year: intervalDay.year,
+    date: intervalDay.toFormat(DATE_FORMAT),
+  }));
 };
 
 exports.getCalendarDays = getCalendarDays;
